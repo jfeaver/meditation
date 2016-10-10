2290c5338cde3ea6baba6baf59e16ba4330ae7d8
Nathan Feaver <nathan.feaver@gmail.com>
2016-10-10 11:21:41 -0600

    add toggle button

--- src/Reading.elm
+++ src/Reading.elm
@@ -1,55 +1,107 @@
 port module Reading exposing (init, view, update, subscriptions)
 
 import Date exposing (Date)
-import Date.Extra.Format
-import Date.Extra.Config.Config_en_us
-import Task
+import Date.Extra.Core
+import Date.Extra.I18n.I_en_us as English
+import Task exposing (Task)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import String
+import Html.Events exposing (onClick)
 
 
 -- MODEL
 
 
 type alias Model =
-    Date
+    { timeOfDay : TimeOfDay
+    , month : Int
+    , day : Int
+    }
+
+
+type TimeOfDay
+    = Morning
+    | Evening
 
 
 initialModel =
-    Date.fromTime 506502000000
+    { timeOfDay = Morning
+    , month = 1
+    , day = 19
+    }
 
 
-init : ( Model, Cmd Action )
+init : ( Model, Cmd Msg )
 init =
-    ( initialModel, (getCurrentDate ChangeDate) )
+    ( initialModel, syncToday )
 
 
-getCurrentDate : (Model -> a) -> Cmd a
-getCurrentDate action =
-    Task.perform (\date -> action date) (\date -> action date) Date.now
+syncToday : Cmd Msg
+syncToday =
+    let
+        success =
+            (\model -> Update model)
+
+        failure =
+            (\model -> Update model)
+    in
+        Task.perform failure success getToday
+
+
+getToday : Task Model Model
+getToday =
+    Task.andThen Date.now (\date -> Task.succeed (modelFromDate date))
+
+
+modelFromDate : Date -> Model
+modelFromDate date =
+    let
+        timeOfDay =
+            if (Date.hour date) < 12 then
+                Morning
+            else
+                Evening
+    in
+        { timeOfDay = timeOfDay
+        , month = Date.Extra.Core.monthToInt (Date.month date)
+        , day = Date.day date
+        }
 
 
 
 -- UPDATE
 
 
-type Action
-    = ChangeDate Model
+type Msg
+    = Update Model
+    | ToggleMorningEvening
 
 
-update : Action -> Model -> ( Model, Cmd Action )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
     case action of
-        ChangeDate model ->
+        Update model ->
             ( model, Cmd.none )
+
+        ToggleMorningEvening ->
+            ( toggleMorningEvening model, Cmd.none )
+
+
+toggleMorningEvening : Model -> Model
+toggleMorningEvening model =
+    case model.timeOfDay of
+        Morning ->
+            { model | timeOfDay = Evening }
+
+        Evening ->
+            { model | timeOfDay = Morning }
 
 
 
 -- SUBSCRIPTIONS
 
 
-subscriptions : Model -> Sub Action
+subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
@@ -58,7 +110,7 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Action
+view : Model -> Html Msg
 view model =
     div
         [ class "container"
@@ -67,6 +119,7 @@ view model =
             [ id "main"
             ]
             [ h2 [] [ text (title model) ]
+            , button [ onClick ToggleMorningEvening ] [ text "toggle" ]
             ]
         , div
             [ id "footer"
@@ -82,31 +135,19 @@ title model =
         ++ ", "
         ++ (month model)
         ++ " "
-        ++ (day model)
+        ++ (toString model.day)
 
 
 timeOfDay : Model -> String
 timeOfDay model =
-    let
-        hour =
-            Date.hour model
-    in
-        if hour < 12 then
+    case model.timeOfDay of
+        Morning ->
             "Morning"
-        else
+
+        Evening ->
             "Evening"
 
 
 month : Model -> String
 month =
-    date_format "%B"
-
-
-day : Model -> String
-day =
-    date_format "%e"
-
-
-date_format : String -> Model -> String
-date_format =
-    Date.Extra.Format.format Date.Extra.Config.Config_en_us.config
+    .month >> Date.Extra.Core.intToMonth >> English.monthName
